ae7b0f6f59acd0444e8826b7cbe7c6d80827cb03
LewisFelipe <aluisiusfeliposu@gmail.com>
2021-09-01 23:03:40 -0300

    Tirando erros comitados

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    float timer;
+    int count = 0;
+
+    void TickTimer()
+    {
+        count += (int)Time.deltaTime;
+        timer = count;
+        //if ()
+    }
+
+    void Update()
+    {
+        TickTimer();
+    }
 }
--- Assets/Scripts/Sites.cs
+++ Assets/Scripts/Sites.cs
@@ -4,6 +4,24 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class Sites : MonoBehaviour
-{
-    Image[2] noticia;
+{/*
+    private Sites Instance;
+    public Sites Singleton
+    {
+        get
+        {
+            if(Instance == null)
+            {
+                instance = FindObjectOfType<Sites>();
+            }
+            return Instance;
+        }
+    }
+    public int quantityImagePairs;
+    public Image[,] noticias;
+
+    void Start()
+    {
+        noticias = new Image[quantityImagePairs, 2]();
+    }*/
 }
