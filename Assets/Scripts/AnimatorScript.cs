39016cbd1ec3c14c35bf286ffffd661a81646486
LuisFilipeTinos <lftinos@gmail.com>
2021-09-02 00:55:44 -0300

    Criação de UI e controle de telas

---
+++ Assets/Scripts/AnimatorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour
+{
+    Animator anim;
+
+    [SerializeField]
+    UIManager uiManager;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (uiManager.canAnimate == true)
+        {
+            anim.Play("AnimConfig");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    GameObject configMenu;
+
+    public bool canAnimate = false;
+
+    private void Start()
+    {
+        configMenu = GameObject.FindGameObjectWithTag("Config");
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void About()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ConfigWindowOpen()
+    {
+        configMenu.SetActive(true);
+        canAnimate = true;
+        
+    }
+
+    public void ConfigWindowClose()
+    {
+        configMenu.SetActive(false);
+    }
+}
